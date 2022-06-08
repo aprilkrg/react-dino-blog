9ede5f284b8c76b8e24ff27e28c994e7b9197d0b
aprilkrg <aprilkrgonzales@gmail.com>
2022-06-08 12:41:56 -0600

    setState in App so Post can update state body

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import Post from "./Post.jsx";
 import React, { Component } from "react";
 
 class App extends Component {
+    // react v16 allows for a simple state obj without a constructor
+    // set state object and give it the same data as `post`
+    state = {
+        title: "Dinosaurs are awesome",
+        author: "Stealthy Stegosaurus",
+        body: "Check out this body property!",
+        comments: ["First!", "Great post", "Hire this author now!"]
+    }
+    // create a function that will change the body of state
+    changeBody = () => {
+        console.log("click")
+        const userInput = prompt("What should the current body be?")
+        console.log(userInput, 'user input')
+        this.setState({
+            body: userInput
+        })
+    }
     render() {
         const post = {
             title: "Dinosaurs are awesome",
@@ -12,7 +29,13 @@ class App extends Component {
         };
         return (
             <div className="App">
-                <Post post={post} name="April" />
+                <Post 
+                    // pass the Post component the post object, state object, and changeBody method
+                    post={post} 
+                    name="April" 
+                    dinoState={this.state} 
+                    changeBody={this.changeBody}
+                />
             </div>
         );
     }
