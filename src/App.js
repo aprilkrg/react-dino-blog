86d01329eecc473dac5a11a65da4c60080c84172
aprilkrg <aprilkrgonzales@gmail.com>
2022-06-08 14:41:04 -0600

    clear out state tests and moodpoints => ready for lesson

--- src/App.js
+++ src/App.js
@@ -3,23 +3,6 @@ import Post from "./Post.jsx";
 import React, { Component } from "react";
 
 class App extends Component {
-    // react v16 allows for a simple state obj without a constructor
-    // set state object and give it the same data as `post`
-    state = {
-        title: "Dinosaurs are awesome",
-        author: "Stealthy Stegosaurus",
-        body: "Check out this body property!",
-        comments: ["First!", "Great post", "Hire this author now!"]
-    }
-    // create a function that will change the body of state
-    changeBody = () => {
-        console.log("click")
-        const userInput = prompt("What should the current body be?")
-        console.log(userInput, 'user input')
-        this.setState({
-            body: userInput
-        })
-    }
     render() {
         const post = {
             title: "Dinosaurs are awesome",
@@ -29,12 +12,9 @@ class App extends Component {
         };
         return (
             <div className="App">
-                <Post 
-                    // pass the Post component the post object, state object, and changeBody method
-                    post={post} 
-                    name="April" 
-                    dinoState={this.state} 
-                    changeBody={this.changeBody}
+                <Post
+                    // pass the Post component the post object
+                    post={post}
                 />
             </div>
         );
--- src/Post.jsx
+++ src/Post.jsx
@@ -4,24 +4,14 @@ import Comment from './Comment'
 
 class Post extends Component {
     render() {
-        console.log(this.state, this.props, 'STATEEEE')
         return (
             <>
-                {/* <h1>PROPS:{this.props.post.title}</h1>
+                <h1>{this.props.post.title}</h1>
                 <h3>{this.props.post.author}</h3>
                 <p>{this.props.post.body}</p>
                 {this.props.post.comments.map(item => (
                     <Comment message={item} />
-                ))} */}
-
-                <h1>STATE:{this.props.dinoState.title}</h1>
-                <h3>{this.props.dinoState.author}</h3>
-                <p>{this.props.dinoState.body}</p>
-                {this.props.dinoState.comments.map(item => (
-                    <Comment message={item} />
                 ))}
-                <button onClick={this.props.changeBody}>edit body</button>
-
             </>
         )
     }
